b718257a330d0c890531541900c79a616f73a1a5
AndreiS <Andrei.Salanovich@sefe.eu>
2025-01-13 13:43:55 +0200

    352976: add parentId column to environments table

--- src/Dorc.Database/Schema Objects/Schemas/deploy/Tables/Environment.table.sql
+++ src/Dorc.Database/Schema Objects/Schemas/deploy/Tables/Environment.table.sql
@@ -11,7 +11,8 @@
     [EnvNote]            NVARCHAR (MAX)   NULL,
     [Description]        NVARCHAR (MAX)   NULL,
     [LastUpdate]         DATETIME         NULL,
-    CONSTRAINT [PK_Environment] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [ParentId]           INT              NULL REFERENCES [deploy].[Environment] (Id), 
+    CONSTRAINT [PK_Environment] PRIMARY KEY CLUSTERED ([Id] ASC), 
 );
 
 
