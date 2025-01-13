52b4be972740e7f2cf703b9807d9cc1b4d0f8ce9
Kirill Suspitsin (External) <Kirill.Suspitsin.ext@sefe.eu>
2025-01-13 14:20:24 +0000

    #352075 Remove PreSQL script

--- src/Dorc.Database/Dorc.Database.PreSQL.sql
+++ src/Dorc.Database/Dorc.Database.PreSQL.sql
@@ -1,10 +1,1 @@
-﻿IF COL_LENGTH('[deploy].[DeploymentResult]', 'StartedTime') IS NULL
-BEGIN
-    ALTER TABLE [deploy].[DeploymentResult]
-    ADD [StartedTime] DATETIMEOFFSET (7) NULL;
-END
-IF COL_LENGTH('[deploy].[DeploymentResult]', 'CompletedTime') IS NULL
-BEGIN
-    ALTER TABLE [deploy].[DeploymentResult]
-    ADD [CompletedTime] DATETIMEOFFSET (7) NULL;
-END
+﻿
