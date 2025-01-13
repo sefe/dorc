b4749a0e5877a92d9244242c0c433a6af42109e2
Kirill Suspitsin (External) <Kirill.Suspitsin.ext@sefe.eu>
2025-01-13 14:12:30 +0000

    #352075 Move new columns to the end of designer table

--- src/Dorc.Database/Schema Objects/Schemas/deploy/Tables/DeploymentResult.table.sql
+++ src/Dorc.Database/Schema Objects/Schemas/deploy/Tables/DeploymentResult.table.sql
@@ -2,9 +2,9 @@
     [Id]                  INT            IDENTITY (1, 1) NOT NULL,
     [DeploymentRequestId] INT            NOT NULL,
     [ComponentId]         INT            NOT NULL,
-    [StartedTime]         DATETIMEOFFSET (7) NULL,
-    [CompletedTime]       DATETIMEOFFSET (7) NULL,
     [Log]                 NVARCHAR (MAX) NULL,
-    [Status]              NVARCHAR (32)  NOT NULL
+    [Status]              NVARCHAR (32)  NOT NULL,
+    [StartedTime]         DATETIMEOFFSET (7) NULL,
+    [CompletedTime]       DATETIMEOFFSET (7) NULL
 );
 
