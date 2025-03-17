6855bfbba76467a36a146fc574c69d8f8c324d05
Andrei Salanovich <Andrei.Salanovich@sefe.eu>
2025-03-17 12:24:07 +0200

    cover auth handler with time profiler

--- src/Dorc.PersistentData/Utils/TimeProfiler.cs
+++ src/Dorc.PersistentData/Utils/TimeProfiler.cs
@@ -1,5 +1,6 @@
 ﻿using log4net;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace Dorc.PersistentData.Utils
@@ -8,6 +9,7 @@ namespace Dorc.PersistentData.Utils
     {
         private readonly Stopwatch _stopwatch;
         private readonly ILog _log;
+        private readonly ILogger _logger;
         private readonly string _methodName;
         private static int _threshold = 2000;
 
@@ -28,14 +30,31 @@ namespace Dorc.PersistentData.Utils
             _stopwatch = Stopwatch.StartNew();
         }
 
+        public TimeProfiler(ILogger log, string methodName, int? threshold = null)
+        {
+            _logger = log;
+            _methodName = methodName;
+            _threshold = threshold ?? _threshold;
+            _stopwatch = Stopwatch.StartNew();
+        }
+
         public void LogTime(string methodName)
         {
             _stopwatch.Stop();
             if (_stopwatch.ElapsedMilliseconds > _threshold)
             {
-                _log.Warn($"[Profiler] {_methodName}.({methodName}) took {_stopwatch.ElapsedMilliseconds} ms");
+                logTimeMethod($"[Profiler] {_methodName}.({methodName}) took {_stopwatch.ElapsedMilliseconds} ms");
             }
             _stopwatch.Restart();
+        }
+
+        private void logTimeMethod(string message)
+        {
+            if (_log != null)
+                _log.Warn(message);
+
+            if (_logger != null)
+                _logger.LogWarning(message);
         }
 
         public void Dispose()
@@ -45,7 +64,7 @@ namespace Dorc.PersistentData.Utils
             _stopwatch.Stop();
             if (_stopwatch.ElapsedMilliseconds > _threshold)
             {
-                _log.Warn($"[Profiler] last part of {_methodName} took {_stopwatch.ElapsedMilliseconds} ms");
+                logTimeMethod($"[Profiler] last part of {_methodName} took {_stopwatch.ElapsedMilliseconds} ms");
             }
         }
     }
