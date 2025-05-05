fe1768a25cb154d5e11fc581487698368f1bfb76
Kirill Suspitsin (External) <Kirill.Suspitsin.ext@sefe.eu>
2025-05-05 11:22:26 +0100

    Add builds that IbProgress state for the correct CI/CD

---
+++ src/Dorc.Core/Models/BuildApiFilter.cs
@@ -0,0 +1,16 @@
+﻿using Org.OpenAPITools.Model;
+
+namespace Dorc.Core.Models
+{
+    public class BuildApiFilter
+    {
+        public Build.StatusEnum? Status { get; set; }
+        public Build.ResultEnum? Result { get; set; }
+
+        public BuildApiFilter(Build.StatusEnum? status, Build.ResultEnum? result)
+        {
+            Status = status;
+            Result = result;
+        }
+    }
+}
