3076a58f1f663bb5b5268037c1b14eeace4c2771
Kirill Suspitsin (External) <Kirill.Suspitsin.ext@sefe.eu>
2025-05-07 12:43:48 +0100

    Add pagination to OS deployment result query

--- src/Dorc.OpenSearchData/Sources/DeploymentLogService.cs
+++ src/Dorc.OpenSearchData/Sources/DeploymentLogService.cs
@@ -1,6 +1,7 @@
 ﻿using Dorc.ApiModel;
 using Dorc.OpenSearchData.Model;
 using Dorc.OpenSearchData.Sources.Interfaces;
+using log4net;
 using OpenSearch.Client;
 
 namespace Dorc.OpenSearchData.Sources
@@ -8,12 +9,16 @@ namespace Dorc.OpenSearchData.Sources
     public class DeploymentLogService : IDeploymentLogService
     {
         private readonly IOpenSearchClient _openSearchClient;
+        private readonly ILog _logger;
 
         private readonly string _deploymentResultIndex;
 
-        public DeploymentLogService(IOpenSearchClient openSearchClient, string deploymentResultIndex)
+        private const int _pageSize = 10;
+
+        public DeploymentLogService(IOpenSearchClient openSearchClient, ILog logger, string deploymentResultIndex)
         {
             _openSearchClient = openSearchClient;
+            _logger = logger;
             _deploymentResultIndex = deploymentResultIndex;
         }
 
@@ -22,23 +27,41 @@ namespace Dorc.OpenSearchData.Sources
             var requestIds = deploymentResults.Select(deploymentResult => deploymentResult.RequestId).Distinct().ToList();
             var deploymentResultIds = deploymentResults.Select(deploymentResult => deploymentResult.Id).Distinct().ToList();
 
-            var logs = _openSearchClient.Search<DeployElasticLog>(s => s
-                                .Index(_deploymentResultIndex)
-                                .Query(q => q
-                                    .Bool(b => b
-                                        .Must(must => must
-                                            .Terms(t => t
-                                                .Field(field => field.deployment_result_id)
-                                                .Terms(deploymentResultIds)),
-                                            must => must
-                                            .Terms(t => t
-                                                .Field(field => field.request_id)
-                                                .Terms(requestIds)))))
-                                .Size(10000));
+            var logs = new List<DeployElasticLog>();
+
+            for (int pageNumber = 1; ; pageNumber++)
+            {
+
+                var searchResult = _openSearchClient.Search<DeployElasticLog>(s => s
+                                    .Index(_deploymentResultIndex)
+                                    .Query(q => q
+                                        .Bool(b => b
+                                            .Must(must => must
+                                                .Terms(t => t
+                                                    .Field(field => field.deployment_result_id)
+                                                    .Terms(deploymentResultIds)),
+                                                must => must
+                                                .Terms(t => t
+                                                    .Field(field => field.request_id)
+                                                    .Terms(requestIds)))))
+                                    .From((pageNumber - 1) * _pageSize)
+                                    .Size(_pageSize));
+
+                if (!searchResult.IsValid)
+                {
+                    _logger.Error($"OpenSearch query exception: {searchResult.OriginalException?.Message}.{Environment.NewLine}Query: {searchResult.DebugInformation}");
+                    return;
+                }
+
+                if (searchResult.Documents != null && searchResult.Documents.Any())
+                    logs.AddRange(searchResult.Documents);
+                else
+                    break;
+            }
 
             foreach (var deploymentResult in deploymentResults)
             {
-                var deploymentResultLogs = logs.Documents.Where(d => d.deployment_result_id == deploymentResult.Id && d.request_id == deploymentResult.RequestId)?.OrderBy(d => d.timestamp);
+                var deploymentResultLogs = logs.Where(d => d.deployment_result_id == deploymentResult.Id && d.request_id == deploymentResult.RequestId)?.OrderBy(d => d.timestamp);
                 if (deploymentResultLogs != null && deploymentResultLogs.Any())
                 {
                     deploymentResult.Log = String.Join(Environment.NewLine, deploymentResultLogs.Select(d => $"[{d.timestamp.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss.ffffff")}]   {d.message}"));
