77f1364428a88912991b499f3ac1483c734cf88a
Andrei Salanovich <Andrei.Salanovich@sefe.eu>
2025-07-17 12:00:26 +0300

    improve performance of other methods with passing hashset for user sids to EF

--- src/Dorc.PersistentData/Sources/RequestsStatusPersistentSource.cs
+++ src/Dorc.PersistentData/Sources/RequestsStatusPersistentSource.cs
@@ -142,6 +142,7 @@ namespace Dorc.PersistentData.Sources
 
         public static IQueryable<DeploymentRequestApiModel> GetDeploymentRequestApiModels(IDeploymentContext context, string userName, List<string> userSids)
         {
+            var sidSet = new HashSet<string>(userSids);
             var reqStatusesQueryable = from req in context.DeploymentRequests
                                        join environment in context.Environments on req.Environment equals
                                            environment.Name
@@ -153,7 +154,7 @@ namespace Dorc.PersistentData.Sources
                                        let permissions =
                                            (from env in context.Environments
                                             join ac in context.AccessControls on env.ObjectId equals ac.ObjectId
-                                            where env.Name == environment.Name && (userSids.Contains(ac.Sid) || ac.Pid != null && userSids.Contains(ac.Pid))
+                                            where env.Name == environment.Name && (sidSet.Contains(ac.Sid) || ac.Pid != null && sidSet.Contains(ac.Pid))
                                             select ac.Allow).ToList()
                                        let isPermissioned =
                                            permissions.Any(p => (p & (int)(AccessLevel.Write | AccessLevel.Owner)) != 0)
