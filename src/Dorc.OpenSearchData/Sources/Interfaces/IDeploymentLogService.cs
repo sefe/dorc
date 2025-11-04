441162d695a9e76df363581350a14edd8ab89180
Andrei Salanovich <Andrei.Salanovich@sefe.eu>
2025-11-04 15:57:13 +0200

    first implementation of loading partial logs on signalr event, backend part

--- src/Dorc.OpenSearchData/Sources/Interfaces/IDeploymentLogService.cs
+++ src/Dorc.OpenSearchData/Sources/Interfaces/IDeploymentLogService.cs
@@ -5,5 +5,9 @@ namespace Dorc.OpenSearchData.Sources.Interfaces
     public interface IDeploymentLogService
     {
         void EnrichDeploymentResultsWithLogs(IEnumerable<DeploymentResultApiModel> deploymentResults);
+        
+        void EnrichDeploymentResultsWithLimitedLogs(IEnumerable<DeploymentResultApiModel> deploymentResults, int maxLogsPerResult = 3);
+        
+        string GetLogsForSingleResult(int requestId, int deploymentResultId);
     }
 }
