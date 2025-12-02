d71b8580aff9cd17b352a3095cdb281c0eed3b2b
Andrei Salanovich <Andrei.Salanovich@sefe.eu>
2025-12-02 15:10:57 +0200

    fix database changes

--- src/Dorc.Database/Schema Objects/Schemas/deploy/Tables/Project.table.sql
+++ src/Dorc.Database/Schema Objects/Schemas/deploy/Tables/Project.table.sql
@@ -6,9 +6,9 @@
     [ArtefactsUrl]          NVARCHAR (512)   NULL,
     [ArtefactsSubPaths]     NVARCHAR (512)   NULL, 
     [ArtefactsBuildRegex]   NVARCHAR(MAX)    NULL, 
+    [SourceDatabaseId]      INT NULL, 
     [TerraformGitRepoUrl]   NVARCHAR (512)   NULL,
     [TerraformSubPath]      NVARCHAR (512)   NULL,
-    [SourceDatabaseId]      INT NULL, 
     CONSTRAINT [FK_Project_ToDatabase] FOREIGN KEY ([SourceDatabaseId]) REFERENCES [dbo].[DATABASE]([DB_ID])
 );
 
