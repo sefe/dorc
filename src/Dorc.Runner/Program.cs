fb9081788265148c9a01bb408db69e28f5bb269c
Sameer Lafeer <Sameer.Lafeer@sefe.eu>
2026-04-24 10:26:43 +0100

    change the type of 'obj' to 'ManagementBaseObject'

--- src/Dorc.Runner/Program.cs
+++ src/Dorc.Runner/Program.cs
@@ -122,7 +122,7 @@ namespace Dorc.Runner
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
             ManagementObjectCollection processList = searcher.Get();
 
-            foreach (ManagementObject obj in processList)
+            foreach (ManagementBaseObject obj in processList)
             {
                 string[] argList = new string[] { string.Empty, string.Empty };
                 int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
